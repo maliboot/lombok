710beed558ff0200866b16baa71ddee656e5861b
stone <shilei_zhang@163.com>
2023-08-15 12:14:05 +0800

    feat(master): Lombok注解组合Of功能

--- src/Annotation/Lombok.php
+++ src/Annotation/Lombok.php
@@ -8,13 +8,14 @@ use Attribute;
 use Hyperf\Di\Annotation\AbstractAnnotation;
 use MaliBoot\Lombok\Contract\GetterAnnotationInterface;
 use MaliBoot\Lombok\Contract\LoggerAnnotationInterface;
+use MaliBoot\Lombok\Contract\OfAnnotationInterface;
 use MaliBoot\Lombok\Contract\SetterAnnotationInterface;
 use MaliBoot\Lombok\Contract\ToArrayAnnotationInterface;
 use MaliBoot\Lombok\Contract\ToCollectionAnnotationInterface;
 use MaliBoot\Lombok\Log\LoggerAnnotationTrait;
 
 #[Attribute(Attribute::TARGET_CLASS)]
-class Lombok extends AbstractAnnotation implements GetterAnnotationInterface, SetterAnnotationInterface, LoggerAnnotationInterface, ToArrayAnnotationInterface, ToCollectionAnnotationInterface
+class Lombok extends AbstractAnnotation implements GetterAnnotationInterface, SetterAnnotationInterface, LoggerAnnotationInterface, ToArrayAnnotationInterface, ToCollectionAnnotationInterface, OfAnnotationInterface
 {
     use LoggerAnnotationTrait;
 }
